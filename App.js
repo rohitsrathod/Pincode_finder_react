a2801a439d57af72d0ca031ac56fe87109cd8b62
Rohit Rathod <102748246+rohitsrathod@users.noreply.github.com>
2024-03-03 18:40:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PincodeLookup from '../src/components/PincodeLookup';
+
+const App = () => {
+  return (
+    <div>
+      <PincodeLookup />
+    </div>
+  );
+};
+
+export default App;
